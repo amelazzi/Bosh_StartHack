de722184c8c80bae57d8b684d64041ca6026e1b2
ZeDRoman <ilgovskiy@mail.ru>
2019-03-09 14:25:11 +0300

    auth

--- app.py
+++ app.py
@@ -1,17 +1,42 @@
-from flask import Flask, request
+import httpauth as httpauth
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from constants import my_ip
+from flask import make_response
+from flask_httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
 
 app = Flask(__name__)
 api = Api(app)
 
+last_message = {}
 
-class Hello(Resource):
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@auth.get_password
+def get_password(username):
+    if username == 'bosch':
+        return 'StartHack'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+class Messages(Resource):
     def get(self):
+        answer = {}
+
         return {'hello':'friend'}
 
 
-api.add_resource(Hello, '/Hello')  # Route_1
+api.add_resource(Messages, '/messages')
 
 if __name__ == '__main__':
     app.run(host = my_ip, port='5002')
